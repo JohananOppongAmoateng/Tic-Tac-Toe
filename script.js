1c522805e118d44374a077dc2c5d1f61a895ab30
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2021-12-27 20:32:38 +0000

    Done

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameboardContainer = document.getElementById("gameboard-container")
+const Player = {
+    username ="",
+    marker = ""
+}
+
+const Gameboard = {
+    gameboard : [["X","O","X"],["O","X","X"],["X","O","O"]]
+}
+
+const GameController = {
+   
+
+}
+
+function render() {
+   
+}
